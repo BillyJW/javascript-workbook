dc0df6b234e186e0079f572f5a43faf18b12d221
Billy Watson <shadowsubz@gmail.com>
2018-10-25 19:03:21 -0500

    first commit

---
+++ 03week/gitOlympics.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const arrNames = ['one', 'two', 'three', 'four', 'five'];
+
+const printListOfOlympians(arr) => {
+    farrNames.forEach(function(element) {
+        console.log(element);
+    }
+}
+
+
+
+printListOfOlympians(arrNames);
